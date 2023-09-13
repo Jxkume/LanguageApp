f8942b0c184bda563c9a9ad3a3129acb1e391a16
jxkume <jhonr@metropolia.fi>
2023-09-13 14:43:57 +0300

    Firebase database luotu ja testattu (mm. kirjoittanut databaseen dataa ja lukenu databasesta dataa.)

--- app/src/main/java/com/example/testi/MainActivity.java
+++ app/src/main/java/com/example/testi/MainActivity.java
@@ -1,9 +1,15 @@
 package com.example.testi;
 
+
+
 import androidx.appcompat.app.AppCompatActivity;
-import android.os.AsyncTask;
+
 import android.os.Bundle;
-import android.util.Log;
+
+
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,5 +17,20 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Kirjoitetaan firebaseen dataa
+        FirebaseDatabase FirebaseDatabase = com.google.firebase.database.FirebaseDatabase.getInstance();
+        DatabaseReference rootRef = FirebaseDatabase.getReference();
+
+        DatabaseReference AgeChild = rootRef.child("Session 1").child("Age");
+
+        Integer dataToWrite = 6;
+        AgeChild.setValue(dataToWrite);
+
+
+        DatabaseReference ColourgameAttempts = rootRef.child("Session 1").child("Colourgame").child("Attempts");
+
+        Integer dataToWriteToColourgame = 30;
+        ColourgameAttempts.setValue(dataToWriteToColourgame);
     }
 }
