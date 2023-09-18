73fc2c9f0b8d3423f1ea115a9eb294b5c9ab4a3c
jxkume <jhonr@metropolia.fi>
2023-09-18 10:19:29 +0300

    Kirjoitettu kommenttejä databaseen koodiin että me kaikki ymmärretään mitä tehdään

--- app/src/main/java/com/example/testi/MainActivity.java
+++ app/src/main/java/com/example/testi/MainActivity.java
@@ -19,18 +19,30 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         // Kirjoitetaan firebaseen dataa
+
+        // Haetaan firebasen instanssi ja tallennetaan sen muuttujaan
         FirebaseDatabase FirebaseDatabase = com.google.firebase.database.FirebaseDatabase.getInstance();
+
+        // Mennään roottiin (tässä tapauksessa https://kieliappi-default-rtdb.europe-west1.firebasedatabase.app/ on meidän root) ja tallennetaan sen muuttujaan
         DatabaseReference rootRef = FirebaseDatabase.getReference();
 
+        // Etsitään Age kenttä tietokannasta
         DatabaseReference AgeChild = rootRef.child("Session 1").child("Age");
 
+        // Asetetaan arvo joka tallentuu Age kenttään
         Integer dataToWrite = 6;
+
+        // Kirjoitetaan dataa Age kenttään
         AgeChild.setValue(dataToWrite);
 
-
+        // Etsitään colourgameAttempts tietokannasta
         DatabaseReference ColourgameAttempts = rootRef.child("Session 1").child("Colourgame").child("Attempts");
 
+        // Asetetaan arvo joka tallentuu colourgameAttempts kenttään
         Integer dataToWriteToColourgame = 30;
+
+        // Kirjoitetaan dataa colourgameAttemptiin dataa
         ColourgameAttempts.setValue(dataToWriteToColourgame);
+
     }
 }
