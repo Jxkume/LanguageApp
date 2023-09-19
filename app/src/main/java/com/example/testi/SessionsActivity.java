53e28ad599335e0d37d375748cf7a9f738d2e6df
Korpi <kirsiito@metropolia.fi>
2023-09-19 23:49:47 +0300

    Sessions and the start of Splash

---
+++ app/src/main/java/com/example/testi/SessionsActivity.java
@@ -0,0 +1,14 @@
+package com.example.testi;
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+
+
+public class SessionsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sessions);
+    }
+
+}
---
+++ app/src/main/java/com/example/testi/SplashActivity.java
@@ -0,0 +1,15 @@
+package com.example.testi;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.os.HandlerCompat;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.window.SplashScreen;
+
+public class SplashActivity extends AppCompatActivity {
+
+
+
+}
