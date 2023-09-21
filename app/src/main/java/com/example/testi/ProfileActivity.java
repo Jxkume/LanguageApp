bc3807eebd69afca2ac53542c8f32fa787e86516
valttku <valtteri.kuitula@metropolia.fi>
2023-09-21 21:08:42 +0300

    Lisätty päänäkymien alanappeihin toiminnallisuus.

--- app/src/main/java/com/example/testi/ProfileActivity.java
+++ app/src/main/java/com/example/testi/ProfileActivity.java
@@ -1,6 +1,9 @@
 package com.example.testi;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.ImageView;
+
 import androidx.appcompat.app.AppCompatActivity;
 
 public class ProfileActivity extends AppCompatActivity{
@@ -8,5 +11,24 @@ public class ProfileActivity extends AppCompatActivity{
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.profileactivity);
+
+        // Haetaan aktiviteetin alanapit
+        ImageView homeIcon = findViewById(R.id.homeIcon);
+        ImageView settingsIcon = findViewById(R.id.settingsIcon);
+
+        // Lisätään nappeihin klikkaustoiminnallisuus
+        homeIcon.setOnClickListener(v -> {
+            // Siirrytään kotiaktiviteettiin
+            Intent home = new Intent(ProfileActivity.this, HomeActivity.class);
+            startActivity(home);
+        });
+
+        settingsIcon.setOnClickListener(v -> {
+            // Siirrytään asetukset-aktiviteettiin
+            Intent settings = new Intent(ProfileActivity.this, SettingsActivity.class);
+            startActivity(settings);
+        });
+
     }
+
 }
