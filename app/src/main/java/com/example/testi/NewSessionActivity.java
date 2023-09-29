1b77af6eb8eb8233b16f04b6a14972576e34efe6
valttku <valtteri.kuitula@metropolia.fi>
2023-09-29 23:58:06 +0300

    Luotu NewSessionActivity ja muokattu sen ulkonäköä. Lisätty aktiviteetti Manifestiin.

---
+++ app/src/main/java/com/example/testi/NewSessionActivity.java
@@ -0,0 +1,16 @@
+package com.example.testi;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class NewSessionActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.newsessionactivity);
+
+    }
+
+}
