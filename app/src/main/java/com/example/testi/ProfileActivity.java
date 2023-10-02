b75ecd9d8bb408b20a7e5005696c0206a6008e5c
valttku <valtteri.kuitula@metropolia.fi>
2023-10-02 21:12:27 +0300

    Pop-upin toiminnallisuus kunnossa, enää puuttuu kuvan tallentaminen tietokantaan. Pop-upin koodi otettu pois profiilinäkymästä, lisätään sitten kun saadaan ensin tallennettua kuva tietokantaan ja haettua se sieltä oikein profiilinäkymään.

--- app/src/main/java/com/example/testi/ProfileActivity.java
+++ app/src/main/java/com/example/testi/ProfileActivity.java
@@ -1,9 +1,7 @@
 package com.example.testi;
 
-import android.app.Dialog;
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.Window;
 import android.widget.ImageView;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -32,23 +30,6 @@ public class ProfileActivity extends AppCompatActivity{
             Intent settings = new Intent(ProfileActivity.this, SettingsActivity.class);
             startActivity(settings);
             overridePendingTransition(0, 0);
-        });
-
-        // Haetaan profiilikuvan tausta pop-uppia varten
-        ImageView profilePictureBackground = findViewById(R.id.profilePictureBackground);
-
-        // Tällä saadaan pop-up näkyville, kun klikataan profiilikuvan taustaa
-        profilePictureBackground.setOnClickListener(v -> {
-
-            final Dialog popUp = new Dialog(ProfileActivity.this);
-
-            popUp.getWindow().setBackgroundDrawableResource(android.R.color.transparent);
-            popUp.getWindow().setDimAmount(0.2f);
-
-            popUp.requestWindowFeature(Window.FEATURE_NO_TITLE);
-            popUp.setContentView(R.layout.profilepicturepopup);
-            popUp.show();
-
         });
 
     }
