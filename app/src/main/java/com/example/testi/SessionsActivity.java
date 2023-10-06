eccc84d85f4e5f25f6c62943484738a6f06a3e3f
valttku <valtteri.kuitula@metropolia.fi>
2023-10-06 19:27:28 +0300

    Muokattu Session-näkymää. Tällä hetkellä onCreate-metodissa haetaan kaikki sessionit tietokannasta ja asetetaan napit oikeanlaisiksi ja käyttäjän nimi niihin.

--- app/src/main/java/com/example/testi/SessionsActivity.java
+++ app/src/main/java/com/example/testi/SessionsActivity.java
@@ -1,24 +1,83 @@
 package com.example.testi;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.ImageView;
+import android.widget.TextView;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 
 public class SessionsActivity extends AppCompatActivity {
 
     private boolean saveslotTaken = false;
+    private ImageView firstSessionButton, secondSessionButton, thirdSessionButton, fourthSessionButton, fifthSessionButton;
+    private TextView firstSessionTextView, secondSessionTextView, thirdSessionTextView, fourthSessionTextView, fifthSessionTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_sessions);
-        setupClickListener1();
-        setupClickListener2();
-        setupClickListener3();
-        setupClickListener4();
-        setupClickListener5();
+
+        this.firstSessionButton = findViewById(R.id.session1_btn);
+        this.secondSessionButton = findViewById(R.id.session2_btn);
+        this.thirdSessionButton = findViewById(R.id.session3_btn);
+        this.fourthSessionButton = findViewById(R.id.session4_btn);
+        this.fifthSessionButton = findViewById(R.id.session5_btn);
+
+        this.firstSessionTextView = findViewById(R.id.firstSessionUsername);
+        this.secondSessionTextView = findViewById(R.id.secondSessionUsername);
+        this.thirdSessionTextView = findViewById(R.id.thirdSessionUsername);
+        this.fourthSessionTextView = findViewById(R.id.fourthSessionUsername);
+        this.fifthSessionTextView = findViewById(R.id.fifthSessionUsername);
+
+        DatabaseReference sessionsRef = FirebaseDatabase.getInstance().getReference().child("Sessions");
+        sessionsRef.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                if (dataSnapshot.exists()) {
+                    for (DataSnapshot sessionSnapshot : dataSnapshot.getChildren()) {
+                        // Haetaan sessionin avain
+                        String sessionKey = sessionSnapshot.getKey();
+                        // Haetaan avaimen sisällä oleva käyttäjän nimi
+                        String username = sessionSnapshot.child("Username").getValue(String.class);
+
+                        if (sessionKey != null) {
+                            if (sessionKey.equals("1")) {
+                                firstSessionButton.setImageResource(R.drawable.oldsession);
+                                firstSessionTextView.setText(username);
+                            } else if (sessionKey.equals("2")) {
+                                secondSessionButton.setImageResource(R.drawable.oldsession);
+                                secondSessionTextView.setText(username);
+                            } else if (sessionKey.equals("3")) {
+                                thirdSessionButton.setImageResource(R.drawable.oldsession);
+                                thirdSessionTextView.setText(username);
+                            } else if (sessionKey.equals("4")) {
+                                fourthSessionButton.setImageResource(R.drawable.oldsession);
+                                fourthSessionTextView.setText(username);
+                            } else if (sessionKey.equals("5")) {
+                                fifthSessionButton.setImageResource(R.drawable.oldsession);
+                                fifthSessionTextView.setText(username);
+                            }
+                        }
+                    }
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+                Log.d("Session", "Sessionien latauksessa tuli virhe");
+            }
+        });
+
     }
 
     private void setupClickListener1() {
