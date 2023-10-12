ba42e9b3c8bb61b434c91931a8a9cce36d41f4c4
valttku <valtteri.kuitula@metropolia.fi>
2023-10-12 18:21:42 +0300

    Poistettu turha testi

--- app/src/androidTest/java/com/example/testi/SplashActivityInstrumentedTest.java
+++ app/src/androidTest/java/com/example/testi/SplashActivityInstrumentedTest.java
@@ -25,17 +25,4 @@ public class SplashActivityInstrumentedTest {
                 .check(ViewAssertions.matches(ViewMatchers.isDisplayed()));
     }
 
-    @Test
-    public void testSplashScreenTransition2() {
-
-        try {
-            Thread.sleep(7000);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
-
-        // Tämän testin ei pitäisi mennä läpi
-        Espresso.onView(ViewMatchers.withId(R.id.profilePictureBackground))
-                .check(ViewAssertions.matches(ViewMatchers.isDisplayed()));
-    }
 }
