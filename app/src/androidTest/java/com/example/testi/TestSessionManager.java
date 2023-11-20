4c9bde92197a7b1ce2d8511a2c3c76034759cde9
valttku <valtteri.kuitula@metropolia.fi>
2023-11-21 01:18:33 +0200

    Luotu TestSessionManager-luokka, jota hyödynnetään testisession luomisessa testejä varten

---
+++ app/src/androidTest/java/com/example/testi/TestSessionManager.java
@@ -0,0 +1,28 @@
+package com.example.testi;
+
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+public class TestSessionManager {
+
+    private static String testSessionKey;
+
+    public static void createTestSession() {
+        Session testSession = new Session(-1, 1, "testUser", 0, 1, 1, "fi");
+        testSessionKey = testSession.getSessionUniqueKey();
+    }
+
+    public static void deleteTestSession() {
+        if (testSessionKey != null) {
+            // Haetaan Session-node tietokannasta
+            DatabaseReference sessionRef = FirebaseDatabase.getInstance().getReference().child("Sessions").child(testSessionKey);
+            // Poistetaan oikea sessio avaimen avulla
+            sessionRef.removeValue();
+        }
+    }
+
+    public static String getTestSessionKey() {
+        return testSessionKey;
+    }
+
+}
