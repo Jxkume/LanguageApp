c4135572c499d5c5306aa63f918c8667b489a6d5
valttku <valtteri.kuitula@metropolia.fi>
2023-11-25 04:28:04 +0200

    Luotu testi, joka testaa kielen vaihtamista espanjan kielestä arabian kieleen

--- app/src/androidTest/java/com/example/testi/SettingsActivityInstrumentedTest.java
+++ app/src/androidTest/java/com/example/testi/SettingsActivityInstrumentedTest.java
@@ -9,10 +9,11 @@ import android.util.Log;
 import androidx.annotation.NonNull;
 import androidx.test.espresso.Espresso;
 import androidx.test.espresso.action.ViewActions;
-import androidx.test.ext.junit.rules.ActivityScenarioRule;
 import androidx.test.espresso.assertion.ViewAssertions;
+import androidx.test.ext.junit.rules.ActivityScenarioRule;
 import androidx.test.espresso.matcher.ViewMatchers;
 
+import com.google.android.gms.tasks.Tasks;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
@@ -25,17 +26,28 @@ import org.junit.Rule;
 import org.junit.Test;
 
 import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.ExecutionException;
 import java.util.concurrent.TimeUnit;
 
 public class SettingsActivityInstrumentedTest {
+
+    private final DatabaseReference sessionsRef = FirebaseDatabase.getInstance().getReference().child("Sessions");
+    private String language = "";
 
     @Rule
     public ActivityScenarioRule<SettingsActivity> activityRule = new ActivityScenarioRule<>(SettingsActivity.class);
 
     @Before
-    public void setUp() {
+    public void setUp() throws InterruptedException {
+        CountDownLatch latch = new CountDownLatch(1);
         // Luodaan testisessio testausta varten
         TestSessionManager.createTestSession();
+        latch.await(2, TimeUnit.SECONDS);
+        // Nyt päivittyy kaikki tiedot tietokannasta :D
+        activityRule.getScenario().recreate();
+        latch.await(2, TimeUnit.SECONDS);
+        // Nyt päivittyy sovelluksen kieli :D
+        activityRule.getScenario().recreate();
     }
 
     @After
@@ -43,7 +55,6 @@ public class SettingsActivityInstrumentedTest {
         // Poistetaan testisessio testien jälkeen
         TestSessionManager.deleteTestSession();
     }
-
 
     @Test
     public void testShowSettings() {
@@ -57,35 +68,50 @@ public class SettingsActivityInstrumentedTest {
                 .check(ViewAssertions.matches(ViewMatchers.isDisplayed()));
     }
 
-    // Sovelluksen kielen vaihtaminen
-    // Testissä sovelluksen kieli on alussa automaattisesti suomen kieli, sillä se on default
+    // Sovelluksen kielen vaihtaminen espanjan kielestä arabian kieleksi
     @Test
-    public void testLanguageChange() throws InterruptedException {
-
-        CountDownLatch latch = new CountDownLatch(1);
-
-        latch.await(5, TimeUnit.SECONDS);
-
-        // Annetaan LanguageManager-luokalle tietoon testisession id
-        LanguageManager.getInstance().setSessionID(-1);
+    public void testLanguageChange() throws InterruptedException, ExecutionException {
 
         // Painetaan aktiviteetissa olevasta lipun kuvasta, joka kertoo sovelluksen nykyisen kielen
         Espresso.onView(ViewMatchers.withId(R.id.flagImageView))
                 .perform(ViewActions.click());
 
-        // Valitaan kielivaihtoehdoista ensimmäinen (espanjan kieli)
-        Espresso.onView(ViewMatchers.withId(R.id.changeLanguageOption1))
+        // Varmistetaan, että näytöllä näkyy pop-up hakemalla kuvanäkymä sen sisältä
+        Espresso.onView(ViewMatchers.withId(R.id.currentFlagInPopUp))
+                .check(matches(isDisplayed()));
+
+        // Valitaan kielivaihtoehdoista toinen (arabian kieli)
+        Espresso.onView(ViewMatchers.withId(R.id.changeLanguageOption2))
                 .perform(ViewActions.click());
 
-        // Odotetaan 5s, jotta kieli ehtii päivittymään tietokantaan
-        latch.await(5, TimeUnit.SECONDS);
+        // Tarkistetaan, että kieli vaihtui arabiaksi varmistamalla yhden tekstinäkymän arvo
+        Espresso.onView(ViewMatchers.withId(R.id.languageTextView))
+                .check(ViewAssertions.matches(ViewMatchers.withText("لغة:")));
+
+        // Odotetaan 10s, että kieli ehtii päivittyä tietokannassa
+        CountDownLatch latch = new CountDownLatch(1);
+        latch.await(10, TimeUnit.SECONDS);
+
+        // Haetaan tietokannasta kielen arvo
+        Tasks.await(sessionsRef.child(TestSessionManager.getTestSessionKey())
+                .child("Language")
+                .get()
+                .addOnSuccessListener(dataSnapshot -> {
+                    if (dataSnapshot.exists()) {
+                        language = dataSnapshot.getValue(String.class);
+                    }
+                })
+                .addOnFailureListener(e -> Log.d("Session", "Kielen hakemisessa tapahtui virhe.")));
+
+        // Varmistetaan, että löydetty kieli on arabian kieli
+        if (!language.equals("ar")) {
+            fail("Tietokannasta ei löytynyt arabian kieltä");
+        }
 
     }
 
     @Test
     public void testSessionDelete() throws InterruptedException {
-
-        DatabaseReference sessionsRef = FirebaseDatabase.getInstance().getReference().child("Sessions");
 
         // Tarkistetaan että päästiin settings aktivitiin
         Espresso.onView(ViewMatchers.withId(R.id.deleteUserButton))
